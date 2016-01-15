f5f9fe711b86d35c63f8f928b5d3023b6fcfd68e
thrbs <bancyo55@hotmail.co.th>
2016-01-16 00:48:34 +0700

    Update and rename .gitignore to infixToPostfix.cpp

---
+++ infixToPostfix.cpp
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack{
+	char ch;
+	struct stack *next;
+}Stack;
+
+Stack *top = NULL;
+Stack *head = NULL;
+Stack *tail = NULL;
+
+void append(char);
+void push(char);
+char pop();
+char topStack();
+int isEmty();
+void compare(char);
+void pushOrCompare(char);
+void check(char);
+void input();
+void output();
+void beforeOutput();
+
+int main(int argc, const char* argv[]){
+	input();
+	beforeOutput();
+	output();
+
+	return 0;
+}
+
+void beforeOutput(){
+	Stack *travel = top;
+	while(travel != NULL){
+		travel = travel->next;
+		append(pop());
+	}
+}
+
+void append(char ch){
+	Stack *n = (Stack *)malloc(sizeof(Stack));
+	if(head == NULL){
+		head = n;
+	}else{
+		tail->next = n;
+	}
+	tail = n;
+	tail->ch = ch;
+	tail->next = NULL;
+}
+
+void push(char ch){
+	Stack *n = (Stack *)malloc(sizeof(Stack));
+	n->next = top;
+	top = n;
+	n->ch = ch;
+}
+
+char pop(){
+	Stack *backup = top;
+	char ch = top->ch;
+	top = top->next;
+	free(backup);
+	return ch;
+}
+
+char topStack(){
+	return (isEmty())? NULL : top->ch;
+}
+
+int isEmty(){
+	return (top == NULL)? 1 : 0;
+}
+
+void compare(char ch){
+	char stackTop = topStack();
+
+	if(stackTop == '(' || ch == '(')
+		push(ch);
+	else if(ch == ')'){
+		Stack *travel = top;
+		while(travel->ch != '('){
+			travel = travel->next;
+			append(pop());
+		}
+		pop();
+	}
+	else if(ch == '^'){
+		if(stackTop == ch){
+			append(pop());
+			pushOrCompare(ch);
+		}else{
+			push(ch);
+		}
+	}else if(ch == '*' || ch == '/'){
+		if(stackTop == '^' || stackTop == '*' || stackTop == '/'){
+			append(pop());
+			pushOrCompare(ch);
+		}else{
+			push(ch);
+		}
+	}
+	else{
+		append(pop());
+		pushOrCompare(ch);
+	}
+}
+
+void pushOrCompare(char ch){
+	if(isEmty())	
+		push(ch);
+	else
+		compare(ch);
+}
+
+void check(char ch){
+	if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')){
+		append(ch);
+	}else{
+		pushOrCompare(ch);
+	}
+}
+
+void input(){
+	char ch;
+	scanf("%c", &ch);
+	while(ch != '\n'){
+		check(ch);
+		scanf("%c", &ch);
+	}
+}
+
+void output(){
+	Stack *travel = head;
+	while(travel != NULL){
+		printf("%c", travel->ch);
+		travel = travel->next;
+	}
+}
